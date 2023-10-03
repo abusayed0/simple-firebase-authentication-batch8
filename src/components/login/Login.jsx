343bcb1fb0bd08d95ebb05fdf6a050b0c233fdd3
abusayed0 <abusayed48876@gmail.com>
2023-10-04 02:04:20 +0600

    added github signin system

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,19 +1,32 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
     const [loggedUser, setLoggedUser] = useState({});
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
             .then(res => {
                 const userData = res.user;
                 // console.log(userData);
                 setLoggedUser(userData);
             })
             .catch(error => console.log(error))
+    };
+
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
+        .then(res => {
+            const userData = res.user;
+            // console.log(userData);
+            setLoggedUser(userData);
+        })
+        .catch(error => {
+            console.log(error);
+        })
     };
 
     const handleLogOut = () => {
@@ -35,7 +48,10 @@ const Login = () => {
                         ?
                         <button onClick={handleLogOut}>Logout</button>
                         :
-                        <button onClick={handleGoogleSignIn}>Google Login</button>
+                        <>
+                            <button onClick={handleGoogleSignIn}>Google Login</button>
+                            <button onClick={handleGithubSignIn}>Github Login</button>
+                        </>
                 }
             </div>
             {
